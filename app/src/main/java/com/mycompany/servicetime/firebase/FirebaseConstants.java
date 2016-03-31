3426743793f81411b4df61fd0f20bcfff11a5b2b
szhx <szhx@mycomputer>
2016-03-30 21:58:02 -0400

    1. Fix FirebaseConstants.java

--- app/src/main/java/com/mycompany/servicetime/firebase/FirebaseConstants.java
+++ app/src/main/java/com/mycompany/servicetime/firebase/FirebaseConstants.java
@@ -13,9 +13,6 @@ public final class FirebaseConstants {
     public static final String FIREBASE_LOCATION_TIMESLOT_ITEMS = "timeSlotItems";
     public static final String FIREBASE_LOCATION_USERS = "users";
     public static final String FIREBASE_LOCATION_TIMESLOT_LISTS = "timeSlotLists";
-    public static final String FIREBASE_LOCATION_USER_LISTS = "userLists";
-    public static final String FIREBASE_LOCATION_USER_FRIENDS = "userFriends";
-    public static final String FIREBASE_LOCATION_LISTS_SHARED_WITH = "sharedWith";
     public static final String FIREBASE_LOCATION_UID_MAPPINGS = "uidMappings";
     public static final String FIREBASE_LOCATION_OWNER_MAPPINGS = "ownerMappings";
     //TODO: a temporary name
@@ -24,31 +21,21 @@ public final class FirebaseConstants {
     /**
      * Constants for Firebase object properties
      */
-    public static final String FIREBASE_PROPERTY_BOUGHT = "bought";
-    public static final String FIREBASE_PROPERTY_BOUGHT_BY = "boughtBy";
+    public static final String FIREBASE_PROPERTY_SERVICE_FLAG = "serviceFlag";
+    public static final String FIREBASE_PROPERTY_TIMESLOT_ID = "timeSlotId";
     public static final String FIREBASE_PROPERTY_LIST_NAME = "listName";
     public static final String FIREBASE_PROPERTY_TIMESTAMP_LAST_CHANGED = "timestampLastChanged";
     public static final String FIREBASE_PROPERTY_TIMESTAMP = "timestamp";
-    public static final String FIREBASE_PROPERTY_ITEM_NAME = "itemName";
+    public static final String FIREBASE_PROPERTY_ITEM_NAME = "name";
     public static final String FIREBASE_PROPERTY_EMAIL = "email";
-    public static final String FIREBASE_PROPERTY_USERS_SHOPPING = "usersShopping";
     public static final String FIREBASE_PROPERTY_USER_HAS_LOGGED_IN_WITH_PASSWORD =
             "hasLoggedInWithPassword";
-    public static final String FIREBASE_PROPERTY_TIMESTAMP_LAST_CHANGED_REVERSE =
-            "timestampLastChangedReverse";
-
 
     /**
      * Constants for Firebase URL
      */
     public static final String FIREBASE_URL = BuildConfig.UNIQUE_FIREBASE_ROOT_URL;
     public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
-    public static final String FIREBASE_URL_USER_LISTS =
-            FIREBASE_URL + "/" + FIREBASE_LOCATION_USER_LISTS;
-    public static final String FIREBASE_URL_USER_FRIENDS =
-            FIREBASE_URL + "/" + FIREBASE_LOCATION_USER_FRIENDS;
-    public static final String FIREBASE_URL_LISTS_SHARED_WITH =
-            FIREBASE_URL + "/" + FIREBASE_LOCATION_LISTS_SHARED_WITH;
     public static final String FIREBASE_URL_TIMESLOT_LISTS =
             FIREBASE_URL + FIREBASE_LOCATION_TIMESLOT_LISTS;
 
