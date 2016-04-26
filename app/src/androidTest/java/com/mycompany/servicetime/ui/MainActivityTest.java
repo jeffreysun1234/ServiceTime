0590b2cff2a62e32eac94a41aa32f976e34ae712
szhx <szhx@mycomputer>
2016-04-26 12:32:09 -0400

    refatcor Main test case

--- app/src/androidTest/java/com/mycompany/servicetime/ui/MainActivityTest.java
+++ app/src/androidTest/java/com/mycompany/servicetime/ui/MainActivityTest.java
@@ -1,6 +1,7 @@
 package com.mycompany.servicetime.ui;
 
 import android.support.test.espresso.contrib.PickerActions;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -19,12 +20,16 @@ import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.isTouchable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Created by szhx on 4/1/2016.
@@ -54,8 +59,8 @@ public class MainActivityTest {
     public void addTimeSlot() {
 
         // There is no data at begin.
-        //onView(withId(android.R.id.empty)).check(matches(isDisplayed()));
-        //onView(withId(R.id.empty_tv)).check(matches(withText("No Time Slots.")));
+        onView(withId(R.id.empty_layout)).check(matches(isDisplayed()));
+        onView(withId(R.id.empty_tv)).check(matches(withText("No Time Slots.")));
 
         // click add icon
         onView(withId(R.id.time_slot_list_add)).perform(click());
@@ -66,33 +71,37 @@ public class MainActivityTest {
         onView(withId(R.id.beginTimePicker)).perform(PickerActions.setTime(9, 30));
         onView(withId(R.id.endTimePicker)).perform(PickerActions.setTime(17, 0));
         onView(withId(R.id.day2InWeekToggleButton)).perform(click());
-        //onView(withId(R.id.saveButton)).perform(click());
+        onView(withId(R.id.time_slot_save)).perform(click());
+
+        // locate to the position 0.
+        onView(withId(R.id.timeSlotListRecyclerView)).perform(RecyclerViewActions.scrollToPosition(0));
 
         // verify the data display correctly
-//        onData(anything()).atPosition(0).onChildView(withId(R.id.nameTextView))
-//                .check(matches(withText("Espresso Test")));
+        onView(withId(R.id.nameTextView)).check(matches(withText("Espresso Test")));
 
-//        // Long click the added item
-//        onData(anything()).atPosition(0).onChildView(withId(R.id.nameTextView))
-//                .perform(longClick());
-//
-//        // change the name of the item
-//        onView(withId(R.id.timeSlotNameEditText))
-//                .perform(clearText(), typeText("Name Changed"), closeSoftKeyboard());
-//        //onView(withId(R.id.saveButton)).perform(click());
-//
-//        // verify the name of the item to be changed
-//        onData(anything()).atPosition(0).onChildView(withId(R.id.nameTextView))
-//                .check(matches(withText("Name Changed")));
-//
-//        // Long click the item again
-//        onData(anything()).atPosition(0).onChildView(withId(R.id.nameTextView))
-//                .perform(longClick());
-//
-//        // verify the delete button to be displayed then click the button
-//        //onView(withId(R.id.deleteButton)).check(matches(isDisplayed())).perform(click());
-//
-//        // verify the item to be deleted.
-//        //onView(withId(R.id.empty_tv)).check(matches(withText("No Time Slots.")));
+        // TODO: verify Edit icon is not display
+        //onView(withId(R.id.edit_item_button)).check(matches(not(isDisplayed())));
+
+        // swipe
+        onView(withId(R.id.nameTextView)).perform(swipeLeft());
+
+        // click Edit icon
+        onView(withId(R.id.edit_item_button)).perform(click());
+
+        // change the name of the item
+        onView(withId(R.id.timeSlotNameEditText))
+                .perform(clearText(), typeText("Name Changed"), closeSoftKeyboard());
+        onView(withId(R.id.time_slot_save)).perform(click());
+
+        // verify the name of the item to be changed
+        onView(withId(R.id.timeSlotListRecyclerView)).perform(RecyclerViewActions.scrollToPosition(0));
+        onView(withId(R.id.nameTextView)).check(matches(withText("Name Changed")));
+
+        // swipe again and click Delete icon
+        onView(withId(R.id.nameTextView)).perform(swipeLeft());
+        onView(withId(R.id.delete_item_button)).perform(click());
+
+        // verify the item to be deleted.
+        onView(withId(R.id.empty_tv)).check(matches(withText("No Time Slots.")));
     }
 }
